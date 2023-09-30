7c3c24b1d85069d5c56bfedc1424d080693e9405
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2023-10-01 01:51:54 +0700

    Add new project ECOCENTER-MUSEUM

--- mail/sendmail.php
+++ mail/sendmail.php
@@ -8,25 +8,27 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-class Mailer{
-    public function sendMail($titleMail,$descMail,$addressMail,$name){
+class Mailer
+{
+    public function sendMail($titleMail, $descMail, $addressMail, $name)
+    {
         $mail = new PHPMailer(true);
-        $mail->CharSet=('UTF-8');
+        $mail->CharSet = ('UTF-8');
         try {
-            
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      
-            $mail->isSMTP();                                            
-            $mail->Host       = 'smtp.gmail.com';                     
-            $mail->SMTPAuth   = true;                                   
-            $mail->Username   = 'eco.museum00@gmail.com';                     
-            $mail->Password   = 'gckjnfxmyqvcwuen';                            
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
-            $mail->Port       = 465;                                    
-        
-         
+
+            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'eco.museum00@gmail.com';
+            $mail->Password   = 'gckjnfxmyqvcwuen';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port       = 465;
+
+
             $mail->setFrom('eco.museum00@gmail.com', 'ECO CENTER');
-            $mail->addAddress($addressMail, $name);    
-            $mail->isHTML(true);                                
+            $mail->addAddress($addressMail, $name);
+            $mail->isHTML(true);
             $mail->Subject = $titleMail;
             $mail->Body    = '
             <html>
@@ -34,7 +36,7 @@ class Mailer{
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
                 </head>
                 <body>
-                '.$descMail.'      
+                ' . $descMail . '      
                 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -43,7 +45,7 @@ class Mailer{
                 </body>
             </html>
         ';
-        
+
             $mail->send();
             echo 'Message has been sent';
         } catch (Exception $e) {
